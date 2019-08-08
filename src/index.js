5610179e5defaef1e78db3c204ef1995cee35c76
Subho Karmakar <isubhokarma@gmail.com>
2019-08-08 07:56:00 +0530

    Revert "Redux Setup done"
    This reverts commit e3a55bbcfc45912884e4c33cec9fee5d2a0e4e86.

--- src/index.js
+++ src/index.js
@@ -3,24 +3,19 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 
-import { Provider } from "react-redux";
-import store from "./store";
-
 import theme from "./utils/theme";
 import GlobalStyles from "./utils/global";
 
 import App from "./App";
 
 ReactDom.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <>
-          <App />
-          <GlobalStyles />
-        </>
-      </ThemeProvider>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <>
+        <App />
+        <GlobalStyles />
+      </>
+    </ThemeProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
