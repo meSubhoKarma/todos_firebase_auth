e3a55bbcfc45912884e4c33cec9fee5d2a0e4e86
Subho Karmakar <isubhokarma@gmail.com>
2019-08-08 07:54:13 +0530

    Redux Setup done

--- src/index.js
+++ src/index.js
@@ -3,19 +3,24 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import theme from "./utils/theme";
 import GlobalStyles from "./utils/global";
 
 import App from "./App";
 
 ReactDom.render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <>
-        <App />
-        <GlobalStyles />
-      </>
-    </ThemeProvider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <>
+          <App />
+          <GlobalStyles />
+        </>
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
