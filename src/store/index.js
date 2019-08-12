146ee3a9334f62f9e204cf7abe9870891bca64c2
Subho Karmakar <isubhokarma@gmail.com>
2019-08-12 07:55:30 +0530

    Activating Redux tool on dev mode only

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,12 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// Activating redux dev tools
+// only on dev mode
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
 // https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
 
 const store = createStore(
